0fab1cc7efe3073d0895d154aca507593ec2fcc7
masonsnider30 <masonsnider30@gmail.com>
2017-03-21 12:30:30 -0400

    Started the Java project with a basic JFrame class

---
+++ SumFunGame/src/GameBoard.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class GameBoard extends JFrame{
+	public GameBoard(){
+		setSize(800,600);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setTitle("Sum Fun Game");
+	}
+}
