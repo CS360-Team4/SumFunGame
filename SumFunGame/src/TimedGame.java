6c283461126e89fe87cd9192f60d680bafd002a4
masonsnider30 <masonsnider30@gmail.com>
2017-04-15 13:24:54 -0400

    Added a timer to the timedGame class and it counts down in the console. Just need to make it update the label and then call the checkWin method and stop the game when it's over.

--- SumFunGame/src/TimedGame.java
+++ SumFunGame/src/TimedGame.java
@@ -1,12 +1,42 @@
-import javax.swing.Timer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimedGame extends GameBoard {
 
 	private Timer timer;
+	private int delay = 1000;
+	private int period = 1000;
+	private int timeSeconds = 300;
 	
 	public TimedGame(String name) {
 		super(name);
-		lblTime.setText("05:00");
+		lblTime.setText("5:00");
+		
+		timer = new Timer();
+		System.out.println(getTimeString());
+		timer.scheduleAtFixedRate(new TimerTask() {
+			public void run(){
+				setTime();
+				String timeString = (getTimeString());
+				System.out.println(timeString);
+			}
+		}, delay, period);
 	}
 	
+	private void setTime() {
+	    if (timeSeconds == 1)
+	        timer.cancel();
+	    timeSeconds--;
+	}	
+	
+	private String getTimeString(){
+		String timeString = "";
+		if(timeSeconds%60 == 0){
+			timeString = (Integer.toString(timeSeconds/60) + ":" + Integer.toString(timeSeconds%60) + "0");
+		}
+		else{
+			timeString = (Integer.toString(timeSeconds/60) + ":" + Integer.toString(timeSeconds%60));
+		}
+		return timeString;
+	}
 }
