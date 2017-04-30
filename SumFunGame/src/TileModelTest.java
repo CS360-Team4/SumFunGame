96b339a24e8014d7e92451acf703f0c40ec2538b
Marie Langford <Marie Langford@10.100.252.135>
2017-04-30 15:56:38 -0400

    Added glass box testing methods

--- SumFunGame/src/TileModelTest.java
+++ SumFunGame/src/TileModelTest.java
@@ -12,6 +12,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TileModelTest {
+	
+	/**********************************************************************
+	 ********************** BLACKBOX TESTING METHODS **********************
+	 **********************************************************************/
 
 	/*
 	 * ********************************************************
@@ -165,6 +169,187 @@ public class TileModelTest {
 		
 		// assert
 		assertEquals(9, result);
+	}
+	
+	/**********************************************************************
+	 ********************** GLASSBOX TESTING METHODS **********************
+	 **********************************************************************/
+	
+	/**
+	 * For getNeighbors(), to step through all options need following test cases:
+	 * 1) All neighbors null
+	 * 2) All neighbors blank
+	 * 3) All neighbors non-null and non-blank
+	 * 
+	 */
+	
+	@Test
+	public void testGetNeighborsAllNeighborsNull() {
+		// all neighbors null
+		TileModel x = new TileModel();
+			
+		
+		x.setNorth(null);
+		x.setSouth(null);
+		x.setEast(null);
+		x.setWest(null);
+		x.setNortheast(null);
+		x.setNorthwest(null);
+		x.setSoutheast(null);
+		x.setSouthwest(null);
+		
+		// execute
+		int result = x.getSumMod();
+		
+		// assert
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testGetNeighborsAllNeighborsBlank() {
+		// all neighbors blank
+		TileModel x = new TileModel();
+		
+		TileModel north = new TileModel();
+		TileModel south = new TileModel();
+		TileModel east = new TileModel();
+		TileModel west = new TileModel();
+		TileModel northeast = new TileModel();
+		TileModel southwest = new TileModel();
+		TileModel northwest = new TileModel();
+		TileModel southeast = new TileModel();
+		
+		north.setBlank();
+		south.setBlank();
+		east.setBlank();
+		west.setBlank();
+		northwest.setBlank();
+		northeast.setBlank();
+		southwest.setBlank();
+		southeast.setBlank();
+			
+		
+		x.setNorth(north);
+		x.setSouth(south);
+		x.setEast(east);
+		x.setWest(west);
+		x.setNortheast(northeast);
+		x.setNorthwest(southwest);
+		x.setSoutheast(northwest);
+		x.setSouthwest(southeast);
+		
+		// execute
+		int result = x.getSumMod();
+		
+		// assert
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testGetNeighborsAllNeighborsReal() {
+		// all neighbors non-blank, non-null
+		TileModel x = new TileModel();
+		
+		TileModel north = new TileModel();
+		TileModel south = new TileModel();
+		TileModel east = new TileModel();
+		TileModel west = new TileModel();
+		TileModel northeast = new TileModel();
+		TileModel southwest = new TileModel();
+		TileModel northwest = new TileModel();
+		TileModel southeast = new TileModel();
+		
+		// All different, just for fun
+		north.setNumber(9);
+		south.setNumber(8);
+		east.setNumber(7);
+		west.setNumber(6);
+		northeast.setNumber(5);
+		southwest.setNumber(4);
+		northwest.setNumber(3);
+		southeast.setNumber(2);
+		
+		// Set all tiles non-blank (should already be, but forcing to be sure)
+		north.setBlank(false);
+		south.setBlank(false);
+		east.setBlank(false);
+		west.setBlank(false);
+		northeast.setBlank(false);
+		northwest.setBlank(false);
+		southeast.setBlank(false);
+		southwest.setBlank(false);
+		
+		x.setNorth(north);
+		x.setSouth(south);
+		x.setEast(east);
+		x.setWest(west);
+		x.setNortheast(northeast);
+		x.setSouthwest(southwest);
+		x.setNorthwest(northwest);
+		x.setSoutheast(southeast);
+		
+		// execute
+		int result = x.getSumMod();
+		
+		// assert
+		assertEquals(4, result);
+	}
+	
+	
+	/**
+	 * For getSumMod(), to step through all options need following test case:
+	 * - At least 1 non-blank neighbor
+	 * - At least 1 blank neighbor
+	 */
+	
+	@Test
+	public void testGetSumModGlassBox() {
+		// at least one neighbor blank, at least one neighbor non-blank
+		TileModel x = new TileModel();
+		
+		TileModel north = new TileModel();
+		TileModel south = new TileModel();
+		TileModel east = new TileModel();
+		TileModel west = new TileModel();
+		TileModel northeast = new TileModel();
+		TileModel southwest = new TileModel();
+		TileModel northwest = new TileModel();
+		TileModel southeast = new TileModel();
+		
+		// All different, just for fun
+		north.setNumber(9);
+		south.setNumber(8);
+		east.setNumber(7);
+		west.setNumber(6);
+		northeast.setNumber(5);
+		southwest.setNumber(4);
+		northwest.setNumber(3);
+		southeast.setNumber(2);
+		
+		// Set half tiles non-blank, other half blank
+		north.setBlank(false);
+		south.setBlank(false);
+		east.setBlank(false);
+		west.setBlank(false);
+		northeast.setBlank(true);
+		northwest.setBlank(true);
+		southeast.setBlank(true);
+		southwest.setBlank(true);
+		
+		x.setNorth(north);
+		x.setSouth(south);
+		x.setEast(east);
+		x.setWest(west);
+		x.setNortheast(northeast);
+		x.setSouthwest(southwest);
+		x.setNorthwest(northwest);
+		x.setSoutheast(southeast);
+		
+		// execute
+		int result = x.getSumMod();
+		
+		// assert
+		assertEquals(0, result);
 	}
 
 }
